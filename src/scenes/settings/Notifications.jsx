2e86721986834cc43228c2b25a6d23d45a50c039
Nyingambe Brian <briannmbe@gmail.com>
2023-05-29 23:30:09 +0300

    add dynamism to the notification datafiles

--- src/scenes/settings/Notifications.jsx
+++ src/scenes/settings/Notifications.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Header } from "./index";
 import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
-import { notificationsData } from "./data";
+import { notificationsData, fullPageData } from "./data";
 
 export default function Notifications() {
   return (
@@ -13,37 +13,41 @@ export default function Notifications() {
       />
 
       {/* content */}
-      <Box pr="30px">
-        <Card
-          sx={{
-            bgcolor: "transparent",
-            border: "2px solid #eee",
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <CardContent>
-            <Typography fontWeight="bold">Emails Settings</Typography>
-            <Typography pr="120px">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam
-              minima saepe amet.
-            </Typography>
-          </CardContent>
-          <CardContent>
-            {notificationsData.map((data) => (
-              <Box mb="10px">
-                <Box display="flex">
-                  {data.icon}
-                  <Typography fontWeight="bold" pl="10px">
-                    {data.title}
-                  </Typography>
+      {fullPageData.map((datafile) => (
+        <Box pr="30px">
+          <Card
+            sx={{
+              bgcolor: "transparent",
+              border: "2px solid #eee",
+              display: "flex",
+              justifyContent: "space-between",
+              mb: "20px",
+            }}
+          >
+            <CardContent>
+              <Typography fontWeight="bold">
+                {datafile.headings.title}
+              </Typography>
+              <Typography pr="120px">
+                {datafile.headings.shortDescrition}
+              </Typography>
+            </CardContent>
+            <CardContent>
+              {datafile.notificationsDataFile.map((data) => (
+                <Box mb="10px">
+                  <Box display="flex">
+                    {data.icon}
+                    <Typography fontWeight="bold" pl="10px">
+                      {data.title}
+                    </Typography>
+                  </Box>
+                  <Typography pl="30px">{data.description}</Typography>
                 </Box>
-                <Typography pl="30px">{data.description}</Typography>
-              </Box>
-            ))}
-          </CardContent>
-        </Card>
-      </Box>
+              ))}
+            </CardContent>
+          </Card>
+        </Box>
+      ))}
     </Box>
   );
 }
--- src/scenes/settings/data.js
+++ src/scenes/settings/data.js
@@ -4,42 +4,74 @@ import ToggleOffOutlinedIcon from "@mui/icons-material/ToggleOffOutlined";
 const toggleOffOutlined = "#B8C1C3";
 const toggleOnOutlined = "blue";
 
-export const notificationsData = [
+export const fullPageData = [
   {
-    title: "Emails Settings",
-    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    headings: {
+      title: "Email Settings",
+      shortDescrition: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Totam minima saepe amet tempora animi nobis fuga error iusto molestias veniam`,
+    },
+
+    notificationsDataFile: [
+      {
+        title: "Emails Settings",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
       Totam minima saepe amet tempora animi nobis fuga error iusto molestias veniam`,
-    icon: <ToggleOnOutlinedIcon sx={{ color: toggleOnOutlined }} />,
-  },
-  {
-    title: "Tips & tutorials",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam minima saepe amet tempora animi nobis.",
-    icon: <ToggleOnOutlinedIcon sx={{ color: toggleOnOutlined }} />,
-  },
-  {
-    title: "User Research",
-    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam minima saepe amet tempora
+        icon: <ToggleOnOutlinedIcon sx={{ color: toggleOnOutlined }} />,
+      },
+      {
+        title: "Tips & tutorials",
+        description:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam minima saepe amet tempora animi nobis.",
+        icon: <ToggleOnOutlinedIcon sx={{ color: toggleOnOutlined }} />,
+      },
+      {
+        title: "User Research",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam minima saepe amet tempora
       animi nobis fuga error iusto molestias veniam, Totam minima saepe amet tempora 
       animi nobis fuga error iusto molestias veniam?`,
-    icon: <ToggleOffOutlinedIcon sx={{ color: toggleOffOutlined }} />,
+        icon: <toggleOnOutlined sx={{ color: toggleOnOutlined }} />,
+      },
+      {
+        title: "Comments",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Totam minima saepe amet tempora animi nobis fuga error iusto molestias veniam?`,
+        icon: <toggleOffOutlined sx={{ color: toggleOffOutlined }} />,
+      },
+      {
+        title: "Reminders",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Totam minima saepe amet tempora animi nobis fuga error iusto molestias veniam?`,
+        icon: <ToggleOffOutlinedIcon sx={{ color: toggleOffOutlined }} />,
+      },
+    ],
   },
   {
-    title: "Comments",
-    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    headings: {
+      title: "Push notifications",
+      shortDescrition: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Totam minima saepe amet tempora animi nobis fuga error iusto molestias veniam`,
+    },
+
+    notificationsDataFile: [
+      {
+        title: "Comments",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
       Totam minima saepe amet tempora animi nobis fuga error iusto molestias veniam?`,
-    icon: <ToggleOnOutlinedIcon sx={{ color: toggleOnOutlined }} />,
-  },
-  {
-    title: "Reminders",
-    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        icon: <ToggleOnOutlinedIcon sx={{ color: toggleOnOutlined }} />,
+      },
+      {
+        title: "Reminders",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
       Totam minima saepe amet tempora animi nobis fuga error iusto molestias veniam?`,
-    icon: <ToggleOffOutlinedIcon sx={{ color: toggleOffOutlined }} />,
-  },
-  {
-    title: "Your frequent activities",
-    description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        icon: <toggleOnOutlined sx={{ color: toggleOnOutlined }} />,
+      },
+      {
+        title: "Your frequent activities",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
       Totam minima saepe amet tempora animi nobis fuga error iusto molestias veniam?`,
-    icon: <ToggleOnOutlinedIcon sx={{ color: toggleOnOutlined }} />,
+        icon: <toggleOffOutlined sx={{ color: toggleOffOutlined }} />,
+      },
+    ],
   },
 ];
